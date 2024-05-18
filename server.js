6a14b3871224544be5b29b17035c5dfccf7d32a7
AnnapurnaNeerukonda <22b01a12b3@svcw.edu.in>
2024-05-18 19:10:57 +0530

    userdetails route created

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-
-
+const cors =require("cors")
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors());
